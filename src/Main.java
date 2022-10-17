b088eed84e960d291440d75cd9f4762b607280e1
Максим <99maks@bk.ru>
2022-10-17 18:45:02 +0300

    Задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+
+    public static final Book[] book = new Book[4];
+
+    public static void getBook() {
+        for (Book book : book) {
+            System.out.println("Книга: " + book.getName() + ", Автор: " + book.getAuthor());
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        book[0] = new Book("Страна Чудес", "А.Б. Глушаков");
+        book[1] = new Book("Приключения вокруг света", "В.Г. Исаев");
+        book[2] = new Book("Альматер", "З.И. Ярушев");
+        book[3] = new Book("Зазеркальне", "Н.Н. Лодырев");
+        getBook();
+        // Задача вариант 5
+
+
+
+
     }
 }
